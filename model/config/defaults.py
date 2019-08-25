e0bdf90143e0496752ba0f522d6e7ea33f9845de
mahanfathi <mahan0fathi@gmail.com>
2019-08-26 03:08:28 +0430

    Tune parameters a bit

--- model/config/defaults.py
+++ model/config/defaults.py
@@ -44,19 +44,19 @@ _C.MUJOCO.POOL_SIZE.REWARD = 4
 # Experience Replay
 # ---------------------------------------------------------------------------- #
 _C.EXPERIENCE_REPLAY = CN()
-_C.EXPERIENCE_REPLAY.SIZE = 2 ** 12
+_C.EXPERIENCE_REPLAY.SIZE = 2 ** 15
 _C.EXPERIENCE_REPLAY.SHUFFLE = True
-_C.EXPERIENCE_REPLAY.ENV_INIT_STATE_NUM = 2 ** 11
+_C.EXPERIENCE_REPLAY.ENV_INIT_STATE_NUM = 2 ** 15 * 3 / 4
 
 # ---------------------------------------------------------------------------- #
 # Solver Configs
 # ---------------------------------------------------------------------------- #
 _C.SOLVER = CN()
-_C.SOLVER.EPOCHS = 100000
+_C.SOLVER.EPOCHS = 1000000
 _C.SOLVER.BATCH_SIZE = 64
 
 _C.SOLVER.BASE_LR = 0.00001
-_C.SOLVER.STD_LR_FACTOR = 0.5
+_C.SOLVER.STD_LR_FACTOR = 0.2
 _C.SOLVER.BIAS_LR_FACTOR = 2
 
 _C.SOLVER.MOMENTUM = 0.9
@@ -85,7 +85,7 @@ _C.LOG.PLOT = CN()
 _C.LOG.PLOT.DISPLAY_PORT = 8097
 _C.LOG.PLOT.ITER_PERIOD = 1000  # effective plotting step is _C.LOG.PERIOD * LOG.PLOT.ITER_PERIOD
 _C.LOG.TESTING = CN()
-_C.LOG.TESTING.ON = True
+_C.LOG.TESTING.ON = False
 _C.LOG.TESTING.ITER_PERIOD = 1000
 _C.LOG.TESTING.COUNT_PER_ITER = 2
 
