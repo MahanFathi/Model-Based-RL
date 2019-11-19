ad80ad3a20eef562058c5599292135b3b219affb
Aleksi Ikkala <aleksi.ikkala@aalto.fi>
2019-11-19 17:01:03 +0200

    Modify configs

--- model/config/defaults.py
+++ model/config/defaults.py
@@ -20,14 +20,21 @@ _C.MODEL.POLICY = CN()
 
 _C.MODEL.POLICY = CN()
 _C.MODEL.POLICY.ARCH = "StochasticPolicy"
+_C.MODEL.POLICY.VARIATIONAL = False
 _C.MODEL.POLICY.MAX_HORIZON_STEPS = 100
 _C.MODEL.POLICY.LAYERS = [64, 64, 32, 8]  # a list of hidden layer sizes for output fc. [] means no hidden
-_C.MODEL.POLICY.NORM_LAYERS = [0, 1, 2]  # should be a list of layer indices, example [0, 1, ...]
+#_C.MODEL.POLICY.NORM_LAYERS = [0, 1, 2]  # should be a list of layer indices, example [0, 1, ...]
 _C.MODEL.POLICY.STD_SCALER = 1e-1
 _C.MODEL.POLICY.SOFT_LOWER_STD_BOUND = 1e-4
 _C.MODEL.POLICY.SOFT_LOWER_STD_THRESHOLD = 1e-1
 _C.MODEL.POLICY.OBS_SCALER = False
 _C.MODEL.POLICY.FORGET_COUNT_OBS_SCALER = 5000
+_C.MODEL.POLICY.BATCH_SIZE = 8
+_C.MODEL.POLICY.GAMMA = 0.99
+_C.MODEL.POLICY.METHOD = "None"
+_C.MODEL.POLICY.INITIAL_LOG_SD = 0.0
+_C.MODEL.POLICY.INITIAL_ACTION_MEAN = 0.0
+_C.MODEL.POLICY.INITIAL_ACTION_SD = 0.1
 
 # ---------------------------------------------------------------------------- #
 # Model Configs
@@ -37,7 +44,6 @@ _C.MUJOCO.ENV = 'InvertedPendulumEnv'
 _C.MUJOCO.ASSETS_PATH = "./mujoco/assets/"
 _C.MUJOCO.REWARD_SCALE = 1
 _C.MUJOCO.CLIP_ACTIONS = True
-_C.MUJOCO.GAMMA = 0.98
 _C.MUJOCO.POOL_SIZE = CN()
 
 # ---------------------------------------------------------------------------- #
@@ -53,7 +59,6 @@ _C.EXPERIENCE_REPLAY.ENV_INIT_STATE_NUM = 2 ** 15 * 3 / 4
 # ---------------------------------------------------------------------------- #
 _C.SOLVER = CN()
 _C.SOLVER.EPOCHS = 1000000
-_C.SOLVER.BATCH_SIZE = 64
 
 _C.SOLVER.BASE_LR = 0.001
 _C.SOLVER.STD_LR_FACTOR = 0.001
@@ -63,6 +68,7 @@ _C.SOLVER.MOMENTUM = 0.9
 
 _C.SOLVER.WEIGHT_DECAY = 0.0005
 _C.SOLVER.WEIGHT_DECAY_BIAS = 0
+_C.SOLVER.ADAM_BETAS = (0.9, 0.999)
 
 # ---------------------------------------------------------------------------- #
 # Output Configs
@@ -74,14 +80,14 @@ _C.OUTPUT.DIR = './output'
 # Log Configs
 # ---------------------------------------------------------------------------- #
 _C.LOG = CN()
-_C.LOG.PERIOD = 100
+_C.LOG.PERIOD = 1
 _C.LOG.PLOT = CN()
 _C.LOG.PLOT.DISPLAY_PORT = 8097
-_C.LOG.PLOT.ITER_PERIOD = 1000  # effective plotting step is _C.LOG.PERIOD * LOG.PLOT.ITER_PERIOD
+_C.LOG.PLOT.ITER_PERIOD = 1  # effective plotting step is _C.LOG.PERIOD * LOG.PLOT.ITER_PERIOD
 _C.LOG.TESTING = CN()
 _C.LOG.TESTING.ON = True
-_C.LOG.TESTING.ITER_PERIOD = 1000
-_C.LOG.TESTING.COUNT_PER_ITER = 2
+_C.LOG.TESTING.ITER_PERIOD = 1
+_C.LOG.TESTING.COUNT_PER_ITER = 1
 _C.LOG.CHECKPOINT_PERIOD = 25000
 
 
