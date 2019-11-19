8fce2a9dcea614cb8bb836e7a19b850b6141a758
Aleksi Ikkala <aleksi.ikkala@aalto.fi>
2019-11-19 17:01:15 +0200

    Test a variational network

--- model/layers/feed_forward.py
+++ model/layers/feed_forward.py
@@ -2,7 +2,10 @@ import torch
 import torch.nn as nn
 from itertools import tee
 from collections import OrderedDict
-
+from torch.nn.parameter import Parameter
+from torch.nn import init
+from torch.nn import functional
+import math
 
 def pairwise(iterable):
     """s -> (s0,s1), (s1,s2), (s2, s3), ..."""
@@ -32,6 +35,7 @@ class FeedForward(nn.Module):
         self._layers = OrderedDict()
         for i, (in_size, out_size) in enumerate(pairwise(self._sizes)):
             # Add linear layer
+            #linear_layer = Variational(in_size, out_size, bias=True)
             linear_layer = nn.Linear(in_size, out_size, bias=True)
             self.__setattr__('linear_layer_{}'.format(str(i)), linear_layer)
             self._layers.update({'linear_layer_{}'.format(str(i)): linear_layer})
@@ -56,3 +60,41 @@ class FeedForward(nn.Module):
         #     out = ac(fc(out))
         out = self.sequential(x)
         return out
+
+
+class Variational(nn.Module):
+    __constants__ = ['bias', 'in_features', 'out_features']
+
+    def __init__(self, in_features, out_features, bias=True):
+        super(Variational, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.weight_mean = Parameter(torch.Tensor(out_features, in_features))
+        self.weight_std = Parameter(torch.Tensor(out_features, in_features))
+        if bias:
+            self.bias_mean = Parameter(torch.Tensor(out_features))
+            self.bias_std = Parameter(torch.Tensor(out_features))
+        else:
+            self.register_parameter('bias_mean', None)
+            self.register_parameter('bias_std', None)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        init.kaiming_uniform_(self.weight_mean, a=math.sqrt(5))
+        init.constant_(self.weight_std, 0.8)
+        if self.bias_mean is not None:
+            fan_in, _ = init._calculate_fan_in_and_fan_out(self.weight_mean)
+            bound = 1 / math.sqrt(fan_in)
+            init.uniform_(self.bias_mean, -bound, bound)
+            init.constant_(self.bias_std, 0.8)
+
+    def forward(self, input):
+        weight = torch.distributions.Normal(self.weight_mean, self.weight_std).rsample()
+        bias = torch.distributions.Normal(self.bias_mean, self.bias_std).rsample()
+        return functional.linear(input, weight, bias)
+
+    def extra_repr(self):
+        return 'in_features={}, out_features={}, bias={}'.format(
+            self.in_features, self.out_features, self.bias is not None
+        )
+
