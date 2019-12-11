02f4e98cd991c0a968e9e293dc6e5b5ee1f72fae
Aleksi Ikkala <aleksi.ikkala@aalto.fi>
2019-12-11 14:04:25 +0200

    Update logger

--- utils/logger.py
+++ utils/logger.py
@@ -1,7 +1,8 @@
 import os
 import logging
 import sys
-
+import csv
+from datetime import datetime
 
 def setup_logger(name, save_dir, txt_file_name='log'):
     logger = logging.getLogger(name)
@@ -19,3 +20,15 @@ def setup_logger(name, save_dir, txt_file_name='log'):
         logger.addHandler(fh)
 
     return logger
+
+
+def save_dict_into_csv(save_dir, file_name, output):
+    try:
+        # Append file_name with datetime
+        file_name = os.path.join(save_dir, file_name + "_{0:%Y-%m-%dT%H:%M:%S}".format(datetime.now()))
+        with open(file_name, "w") as file:
+            writer = csv.writer(file)
+            writer.writerow(output.keys())
+            writer.writerows(zip(*output.values()))
+    except IOError:
+        print("Failed to save file {}".format(file_name))
