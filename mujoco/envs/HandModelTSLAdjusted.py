98283e7b8a11792a03e5e64203cfc88938a53b00
Aleksi Ikkala <aleksi.ikkala@aalto.fi>
2020-01-13 10:06:16 +0200

    Update HandModel env

--- mujoco/envs/HandModelTSLAdjusted.py
+++ mujoco/envs/HandModelTSLAdjusted.py
@@ -6,13 +6,16 @@ import xmltodict
 from how_bots_type import MocapWithKeyStateData
 import Utils
 from pyquaternion import Quaternion
+from copy import deepcopy
+
 
 class HandModelTSLAdjustedEnv(mujoco_env.MujocoEnv, utils.EzPickle):
     """
     """
-    def __init__(self):
+    def __init__(self, cfg):
 
         self.initialised = False
+        self.cfg = cfg
 
         # Get model path
         mujoco_assets_dir = os.path.abspath("./mujoco/assets/")
@@ -33,10 +36,16 @@ class HandModelTSLAdjustedEnv(mujoco_env.MujocoEnv, utils.EzPickle):
         T = np.matmul(T_torso, T_keyboard)
 
         # Now create the keyboard object
+        #keyboard = {"@name": "keyboard",
+        #            "@pos": Utils.array_to_string(T[:3, 3]),
+        #            "@quat": Utils.array_to_string(Quaternion(matrix=T).elements),
+        #            "geom": {"@type": "box", "@pos": "0.25 0.005 -0.15", "@size": "0.25 0.005 0.15"}}
+
         keyboard = {"@name": "keyboard",
-                    "@pos": Utils.array_to_string(T[:3, 3]),
-                    "@quat": Utils.array_to_string(Quaternion(matrix=T).elements),
-                    "geom": {"@type": "box", "@pos": "0.25 0.005 -0.15", "@size": "0.25 0.005 0.15"}}
+                    "@pos": Utils.array_to_string(pos + np.array([0.5, 0, 0])),
+                    "@quat": torso["@quat"],
+                    "geom": {"@type": "sphere", "@size": "0.02 0 0", "@rgba": "0.9 0.1 0.1 1"}}
+
 
         # Add it to the xml file
         if not isinstance(p["mujoco"]["worldbody"]["body"], list):
@@ -72,7 +81,20 @@ class HandModelTSLAdjustedEnv(mujoco_env.MujocoEnv, utils.EzPickle):
             f.write(xmltodict.unparse(p, pretty=True, indent="  "))
 
         # Load model
-        mujoco_env.MujocoEnv.__init__(self, mujoco_xml_file_modified, 1)
+        self.frame_skip = self.cfg.MODEL.FRAME_SKIP
+        mujoco_env.MujocoEnv.__init__(self, mujoco_xml_file_modified, self.frame_skip)
+
+        # Set timestep
+        if cfg.MODEL.TIMESTEP > 0:
+            self.model.opt.timestep = cfg.MODEL.TIMESTEP
+
+        # The default initial state isn't stable; run a simulation for a couple of seconds to get a stable initial state
+        duration = 4
+        for _ in range(int(duration/self.model.opt.timestep)):
+            self.sim.step()
+
+        # Use these joint values for initial state
+        self.init_qpos = deepcopy(self.data.qpos)
 
         # Reset model
         self.reset_model()
@@ -84,7 +106,7 @@ class HandModelTSLAdjustedEnv(mujoco_env.MujocoEnv, utils.EzPickle):
 
         # Get positions of each fingertip, maybe start with just 60 seconds
         fps = 24
-        length = 3
+        length = 10
         self.fingers = ["thumb", "index", "middle", "ring", "little"]
         fingers_coords = mocap_ds.fingers.isel(time=range(0, length*fps)).sel(hand='right', joint='tip', finger=self.fingers).values
 
@@ -130,13 +152,17 @@ class HandModelTSLAdjustedEnv(mujoco_env.MujocoEnv, utils.EzPickle):
         self.do_simulation(a, self.frame_skip)
 
         # Cost is difference between target and simulated fingertip positions
-        err = {x: 0 for x in self.fingers}
-        for finger in self.fingers:
-            err[finger] = np.linalg.norm(self.fingers_targets[finger][self._step_idx, :] -
-                                         self.data.site_xpos[self.model._site_name2id["fingertip_"+finger], :], ord=2)
+        #err = {x: 0 for x in self.fingers}
+        #for finger in self.fingers:
+        #    err[finger] = np.linalg.norm(self.fingers_targets[finger][self._step_idx, :] -
+        #                                 self.data.site_xpos[self.model._site_name2id["fingertip_"+finger], :], ord=2)
+
+        err = {}
+        cost = np.linalg.norm(self.data.body_xpos[self.model._body_name2id["keyboard"], :] -
+                              self.data.site_xpos[self.model._site_name2id["fingertip_index"], :], ord=2)
 
         # Product of errors? Or sum? Squared errors?
-        cost = np.prod(list(err.values()))
+        #cost = np.prod(list(err.values()))
 
         return self._get_obs(), -cost, False, err
 
@@ -152,6 +178,7 @@ class HandModelTSLAdjustedEnv(mujoco_env.MujocoEnv, utils.EzPickle):
         #    self.init_qpos + self.np_random.uniform(low=-1, high=1, size=self.model.nq),
         #    self.init_qvel + self.np_random.uniform(low=-1, high=1, size=self.model.nv)
         #)
+        self.sim.reset()
         self.set_state(self.init_qpos, self.init_qvel)
         return self._get_obs()
 
