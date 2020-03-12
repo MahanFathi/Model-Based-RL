e3abb8ea520e99ab491d8d84083ea2b5b056784d
Aleksi Ikkala <aleksi.ikkala@aalto.fi>
2020-03-12 13:46:27 +0200

    Update env

--- mujoco/envs/swimmer.py
+++ mujoco/envs/swimmer.py
@@ -26,7 +26,8 @@ class SwimmerEnv(mujoco_env.MujocoEnv, utils.EzPickle):
         xposafter = self.sim.data.qpos[0]
         reward_fwd = (xposafter - xposbefore) / self.dt
         reward_ctrl = - ctrl_cost_coeff * np.square(a).sum()
-        reward = reward_fwd# + reward_ctrl
+        reward = reward_fwd + reward_ctrl
+
         ob = self._get_obs()
         return ob, reward, False, dict(reward_fwd=reward_fwd, reward_ctrl=reward_ctrl)
 
